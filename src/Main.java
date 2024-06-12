b5a8cb6d4e65ed45e052f7064253d79fd2e81887
Letícia Silva <leticiassb99@gmail.com>
2024-06-12 16:52:37 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso("Curso Java", "Descrição curso Java", 8);
+        Curso curso2 = new Curso("Curso JavaScript", "Descrição Curso JavaScript", 4);
+
+        Mentoria mentoria1 = new Mentoria("Mentoria de Java", "Descrição Mentoria Java", LocalDate.now());
+        Mentoria mentoria2 = new Mentoria("Mentoria de JavaScript", "Descrição Mentoria JavaScript", LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
+
+
     }
 }
